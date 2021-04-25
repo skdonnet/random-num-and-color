0976ef929c941f71f397bfbc1ff1fdd4ce177e96
skdonnet <donovan.ruebusch@student.swic.edu>
2021-04-25 15:51:37 -0500

    create variables and event listeners

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
+const min = document.getElementById('minimum');
+const max = document.getElementById('maximum');
+const button = document.getElementById('getRandNum');
 
+const p = document.createElement('p');
+
+document.body.appendChild(p);
+
+document.min.addEventListener('input', event => {
+  Math.floor = event.value;
+});
+
+document.max.addEventListener('input', event => {
+  Math.ceil = event.value;
+});
+
+document.addEventListener('click', event => {
+  Math.floor(Math.random() * (max - min + 1) + min);
+});
