7e0deda01179c5674f6d388d6a612880d29baa1e
skdonnet <donovan.ruebusch@student.swic.edu>
2021-04-25 20:03:57 -0500

    html spaces, setting up rand

--- scripts.js
+++ scripts.js
@@ -1,19 +1,32 @@
-const min = document.getElementById('minimum');
-const max = document.getElementById('maximum');
-const button = document.getElementById('getRandNum');
-
+const root = document.querySelector(':root');
+const minOld = document.getElementById('minimum');
+const maxOld = document.getElementById('maximum');
 const p = document.createElement('p');
+let minNew = 0;
+let maxNew = 0;
 
-document.body.appendChild(p);
-
-document.min.addEventListener('input', event => {
-  Math.floor = event.value;
+document.querySelector('#minButton').addEventListener('click', event => {
+  minNew = minOld.value;
 });
 
-document.max.addEventListener('input', event => {
-  Math.ceil = event.value;
+document.querySelector('#maxButton').addEventListener('click', event => {
+  maxNew = maxOld.value;
 });
 
-document.addEventListener('click', event => {
-  Math.floor(Math.random() * (max - min + 1) + min);
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // mdn
+}
+
+document.querySelector('#getRandNum').addEventListener('click', event => {
+  const newNum = getRandomIntInclusive(minNew, maxNew);
+  p.innerText = newNum;
+  console.log(newNum);
 });
+
+document
+  .querySelector('input[type="color"]')
+  .addEventListener('input', event => {
+    root.style.setProperty('--color', `${event.target.value}`);
+  });
